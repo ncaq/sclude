c0e07ebfb45fc71d7ca77cbed9d97449919d27bb
ncaq <ncaq@ncaq.net>
2018-08-11 22:43:16 +0900

    added: scanRight … findIndices

--- __tests__/Array.test.ts
+++ __tests__/Array.test.ts
@@ -28,15 +28,15 @@ describe("Array", () => {
     expect([1, 2, 3].uncons).toEqual(Optional.ofNonNull([1, [2, 3]]));
   });
 
-  test("null", () => {
-    expect([].null).toBe(true);
-    expect([1, 2, 3].null).toBe(false);
+  test("isEmpty", () => {
+    expect([].isEmpty).toBe(true);
+    expect([1, 2, 3].isEmpty).toBe(false);
   });
 
   test("subsequences", () => {
     expect([].subsequences).toEqual([[]]);
     expect([0, 1].subsequences).toEqual([[], [0], [1], [0, 1]]);
-    expect(Array.from("abc").subsequences.fmap(x => x.join(""))).toEqual([
+    expect(Array.from("abc").subsequences.map(x => x.join(""))).toEqual([
       "",
       "a",
       "b",
@@ -51,7 +51,7 @@ describe("Array", () => {
   test("permutations", () => {
     expect([].permutations).toEqual([[]]);
     expect([0, 1].permutations).toEqual([[0, 1], [1, 0]]);
-    expect(Array.from("abc").permutations.fmap(x => x.join(""))).toEqual([
+    expect(Array.from("abc").permutations.map(x => x.join(""))).toEqual([
       "abc",
       "bac",
       "cba",
@@ -61,7 +61,36 @@ describe("Array", () => {
     ]);
   });
 
-  test("fmap", () => expect([0, 1, 2].fmap(x => x * 2)).toEqual([0, 2, 4]));
+  test("group", () =>
+    expect(Array.from("Mississippi").group.map(t => t.join(""))).toEqual([
+      "M",
+      "i",
+      "ss",
+      "i",
+      "ss",
+      "i",
+      "pp",
+      "i"
+    ]));
+
+  test("inits", () => {
+    expect([].inits).toEqual([[]]);
+    expect(Array.from("abc").inits.map(t => t.join(""))).toEqual([
+      "",
+      "a",
+      "ab",
+      "abc"
+    ]);
+  });
+
+  test("tails", () => {
+    expect(Array.from("abc").tails.map(t => t.join(""))).toEqual([
+      "abc",
+      "ab",
+      "a",
+      ""
+    ]);
+  });
 
   test("intersperse", () =>
     expect([0, 1, 2].intersperse(0.5)).toEqual([0, 0.5, 1, 0.5, 2]));
@@ -69,9 +98,161 @@ describe("Array", () => {
   test("scan", () =>
     expect([1, 2, 3].scan((a, b) => a + b, 0)).toEqual([0, 1, 3, 6]));
 
+  test("scanRight", () =>
+    expect([1, 2, 3].scanRight((a, b) => a + b, 0)).toEqual([6, 5, 3, 0]));
+
+  test("take", () =>
+    expect(
+      Array.from("Hello World!")
+        .take(5)
+        .join("")
+    ).toEqual("Hello"));
+
+  test("drop", () =>
+    expect(
+      Array.from("Hello World!")
+        .drop(6)
+        .join("")
+    ).toEqual("World!"));
+
+  test("splitAt", () => {
+    expect(Array.from("Hello World!").splitAt(6)).toEqual([
+      Array.from("Hello "),
+      Array.from("World!")
+    ]);
+    expect([1, 2, 3, 4, 5].splitAt(3)).toEqual([[1, 2, 3], [4, 5]]);
+    expect([1, 2, 3].splitAt(3)).toEqual([[1, 2, 3], []]);
+  });
+
   test("takeWhile", () => {
     expect([1, 2, 3, 4, 1, 2, 3, 4].takeWhile(e => e < 3)).toEqual([1, 2]);
     expect([1, 2, 3].takeWhile(e => e < 9)).toEqual([1, 2, 3]);
     expect([1, 2, 3].takeWhile(e => e < 0)).toEqual([]);
+  });
+
+  test("dropWhile", () => {
+    expect([1, 2, 3, 4, 5, 1, 2, 3].dropWhile(e => e < 3)).toEqual([
+      3,
+      4,
+      5,
+      1,
+      2,
+      3
+    ]);
+  });
+
+  test("dropWhileEnd", () => {
+    expect(
+      Array.from("foo ")
+        .dropWhileEnd(e => e === " ")
+        .join("")
+    ).toEqual("foo");
+    expect(
+      Array.from("foo bar")
+        .dropWhileEnd(e => e === " ")
+        .join("")
+    ).toEqual("foo bar");
+  });
+
+  test("span", () => {
+    expect([1, 2, 3, 4, 1, 2, 3, 4].span(e => e < 3)).toEqual([
+      [1, 2],
+      [3, 4, 1, 2, 3, 4]
+    ]);
+  });
+
+  test("break", () => {
+    expect([1, 2, 3, 4, 1, 2, 3, 4].break(e => 3 < e)).toEqual([
+      [1, 2, 3],
+      [4, 1, 2, 3, 4]
+    ]);
+  });
+
+  test("stripPrefix", () => {
+    expect(
+      Array.from("foobar")
+        .stripPrefix(Array.from("foo"))
+        .map(t => t.join(""))
+    ).toEqual(Optional.ofNonNull("bar"));
+    expect(
+      Array.from("foo")
+        .stripPrefix(Array.from("foo"))
+        .map(t => t.join(""))
+    ).toEqual(Optional.ofNonNull(""));
+    expect(
+      Array.from("barfoo")
+        .stripPrefix(Array.from("foo"))
+        .map(t => t.join(""))
+    ).toEqual(Optional.empty());
+    expect(
+      Array.from("barfoobuz")
+        .stripPrefix(Array.from("foo"))
+        .map(t => t.join(""))
+    ).toEqual(Optional.empty());
+  });
+
+  test("isPrefixOf", () => {
+    expect(Array.from("Hello").isPrefixOf(Array.from("Hello World!"))).toBe(
+      true
+    );
+    expect(Array.from("Hello").isPrefixOf(Array.from("Wello World!"))).toBe(
+      false
+    );
+    expect([1].isPrefixOf([])).toBe(false);
+    const prefix: number[] = [];
+    expect(prefix.isPrefixOf([1])).toBe(true);
+  });
+
+  test("isSuffixOf", () => {
+    expect(Array.from("ld!").isSuffixOf(Array.from("Hello World!"))).toBe(true);
+    expect(Array.from("World").isSuffixOf(Array.from("Hello World!"))).toBe(
+      false
+    );
+    expect([1].isSuffixOf([])).toBe(false);
+    const suffix: number[] = [];
+    expect(suffix.isSuffixOf([1])).toBe(true);
+  });
+
+  test("isInfixOf", () => {
+    expect(
+      Array.from("Haskell").isInfixOf(Array.from("I really like Haskell."))
+    ).toBe(true);
+    expect(
+      Array.from("Ial").isInfixOf(Array.from("I really like Haskell."))
+    ).toBe(false);
+    expect([1].isInfixOf([])).toBe(false);
+    const infix: number[] = [];
+    expect(infix.isSuffixOf([1])).toBe(true);
+  });
+
+  test("isSubsequencesOf", () => {
+    expect(
+      Array.from("GHC").isSubsequenceOf(
+        Array.from("The Glorious Haskell Compiler")
+      )
+    ).toBe(true);
+    expect([1, 2, 3].isSubsequenceOf([3, 2, 1, 0])).toBe(false);
+  });
+
+  test("partition", () => {
+    expect(
+      Array.from("Hello World!")
+        .partition(e => Array.from("aeiou").includes(e))
+        .map(e => e.join(""))
+    ).toEqual(["eoo", "Hll Wrld!"]);
+  });
+
+  test("includesIndex", () => {
+    expect([0, 1, 2, 3, 4].includesIndex(4)).toEqual(Optional.ofNonNull(4));
+  });
+
+  test("includesIndices", () => {
+    expect(Array.from("Hello World").includesIndices("o")).toEqual([4, 7]);
+  });
+
+  test("findLastIndex", () => {
+    expect([0].findLastIndex(e => e === 100)).toBe(-1);
+    expect([0, 1].findLastIndex(e => e === 1)).toBe(1);
+    expect([0, 1, 1].findLastIndex(e => e === 1)).toBe(2);
   });
 });
--- src/Array.ts
+++ src/Array.ts
@@ -4,8 +4,11 @@ export function cons<T>(a: T, d: T[]): T[] {
   return [a].concat(d);
 }
 
+export function snoc<T>(d: T[], a: T): T[] {
+  return d.concat(a);
+}
+
 declare global {
-  // 主にData.Listから
   interface Array<T> {
     // ++ is concat
     readonly head: Optional<T>;
@@ -13,7 +16,15 @@ declare global {
     readonly tail: T[];
     readonly init: T[];
     readonly uncons: Optional<[T, T[]]>;
-    readonly null: boolean;
+    /**
+     * Haskellのexportには存在しませんが有用
+     */
+    readonly unsnoc: Optional<[T[], T]>;
+    /**
+     * HaskellではnullですがJavaScriptの予約語と被って明らかにややこしいのでisEmpty
+     * emptyは空の値とややこしいので避けました
+     */
+    readonly isEmpty: boolean;
     // length is length
     readonly subsequences: T[][];
     readonly permutations: T[][];
@@ -23,8 +34,10 @@ declare global {
     // productを型安全に定義する方法がわからない
     // maximumを型安全に定義する方法がわからない
     // minimumを型安全に定義する方法がわからない
+    readonly group: T[][];
+    readonly inits: T[][];
+    readonly tails: T[][];
 
-    fmap<R>(mapper: (x: T) => U): U[];
     intersperse(inter: T): T[];
     // intercalateを型安全に定義する方法がわからない
     // transposeを型安全に定義する方法がわからない
@@ -37,7 +50,53 @@ declare global {
       callback: ((accumulator: U, currentValue: T) => U),
       initialValue: U
     ): U[];
+    scanRight<U>(
+      callback: (previousValue: U, currentValue: T) => U,
+      initialValue: U
+    ): U[];
+    // mapAccumLはTraversableを表現する方法がわからない
+    // iterate, repeat, cycleは無限配列が標準に含まれていない
+    // replicateは自然にメソッドに出来ない
+    // unfoldrはメソッドにできない
+    take(size: number): T[];
+    drop(size: number): T[];
+    splitAt(point: number): [T[], T[]];
     takeWhile(pred: (x: T) => boolean): T[];
+    dropWhile(pred: (x: T) => boolean): T[];
+    dropWhileEnd(pred: (x: T) => boolean): T[];
+    span(pred: (x: T) => boolean): [T[], T[]];
+    break(pred: (x: T) => boolean): [T[], T[]];
+    stripPrefix(prefix: T[]): Optional<T[]>;
+    isPrefixOf(body: T[]): boolean;
+    isSuffixOf(body: T[]): boolean;
+    isInfixOf(body: T[]): boolean;
+    isSubsequenceOf(body: T[]): boolean;
+    // elem is includes
+    // lookupは特殊化出来ない
+    // find is find
+    // filter is filter
+    partition(pred: (x: T) => boolean): [T[], T[]];
+    // !! is []
+    /**
+     * HaskellではelemIndex
+     * JavaScriptではelem相当の機能はincludesなので名前を揃える
+     */
+    includesIndex(target: T): Optional<number>;
+    includesIndices(target: T): number[];
+    // findIndex is findIndex
+    /*
+     * `findLastIndex()`関数は配列内の要素が指定されたテスト関数を満たす場合
+     * 配列内のインデックスを返します
+     * そうでない場合は`-1`を返します
+     * 配列は逆向きに検索されます
+     * HaskellのPreludeに存在しない関数ですが有用なので追加
+     * `findIndex`と対称性を保つために`Optional`はあえて使っていません
+     * @param {function} pred 配列内の各要素に対して実行する関数です。1個の引数を取ります
+     * @returns {number} 条件を満たすインデックス 見つからなかったら`-1`
+     */
+    findLastIndex(pred: (x: T) => boolean): number;
+    findIndices(pred: (x: T) => boolean): number[];
+    groupBy(pred: (a: T, b: T) => boolean): T[][];
   }
 }
 
@@ -66,15 +125,24 @@ Object.defineProperty(Array.prototype, "init", {
 });
 
 Object.defineProperty(Array.prototype, "uncons", {
-  get() {
-    if (0 < this.length) {
-      return Optional.ofNonNull([this[0], this.tail]);
-    }
-    return Optional.empty();
+  get<T>() {
+    return this.head.matches({
+      empty: () => Optional.empty(),
+      present: (x: T) => Optional.ofNonNull([x, this.tail])
+    });
   }
 });
 
-Object.defineProperty(Array.prototype, "null", {
+Object.defineProperty(Array.prototype, "unsnoc", {
+  get<T>() {
+    return this.last.matches({
+      empty: () => Optional.empty(),
+      present: (x: T) => Optional.ofNonNull([this.init, x])
+    });
+  }
+});
+
+Object.defineProperty(Array.prototype, "isEmpty", {
   get() {
     return this.length === 0;
   }
@@ -153,9 +221,29 @@ Object.defineProperty(Array.prototype, "or", {
   }
 });
 
-Array.prototype.fmap = function(mapper) {
-  return this.map(e => mapper(e));
-};
+Object.defineProperty(Array.prototype, "group", {
+  get<T>() {
+    return this.groupBy((a: T, b: T) => a === b);
+  }
+});
+
+Object.defineProperty(Array.prototype, "inits", {
+  get<T>() {
+    return this.unsnoc.matches({
+      empty: () => [[]],
+      present: ([xs, _]: [T[], T]) => snoc(xs.inits, [this])
+    });
+  }
+});
+
+Object.defineProperty(Array.prototype, "tails", {
+  get<T>() {
+    return this.unsnoc.matches({
+      empty: () => [[]],
+      present: ([xs, _]: [T[], T]) => cons(this, xs.tails)
+    });
+  }
+});
 
 Array.prototype.intersperse = function<T>(inter: T) {
   const result: T[] = [];
@@ -176,11 +264,162 @@ Array.prototype.scan = function(callback, initialValue) {
   });
 };
 
+Array.prototype.scanRight = function(callback, initialValue) {
+  return this.uncons.matches({
+    empty: () => [initialValue],
+    present: ([x, xs]) => {
+      const r = xs.scanRight(callback, initialValue);
+      return cons(callback(x, r[0]), r);
+    }
+  });
+};
+
+Array.prototype.take = function(size) {
+  return this.slice(0, size);
+};
+
+Array.prototype.drop = function(size) {
+  return this.slice(size);
+};
+
+Array.prototype.splitAt = function(point) {
+  return [this.take(point), this.drop(point)];
+};
+
 Array.prototype.takeWhile = function(pred) {
-  const takeIndex = this.findIndex(e => !pred(e));
-  if (takeIndex === -1) {
+  const index = this.findIndex(e => !pred(e));
+  if (index === -1) {
     return [...this];
   } else {
-    return this.slice(0, takeIndex);
+    return this.take(index);
   }
+};
+
+Array.prototype.dropWhile = function(pred) {
+  const index = this.findIndex(e => !pred(e));
+  if (index === -1) {
+    return [...this];
+  } else {
+    return this.drop(index);
+  }
+};
+
+Array.prototype.dropWhileEnd = function(pred) {
+  return this.reduceRight(
+    (previousValue, currentValue) =>
+      pred(currentValue) && previousValue.isEmpty
+        ? []
+        : cons(currentValue, previousValue),
+    []
+  );
+};
+
+Array.prototype.span = function(pred) {
+  return [this.takeWhile(pred), this.dropWhile(pred)];
+};
+
+Array.prototype.break = function(pred) {
+  return this.span(e => !pred(e));
+};
+
+Array.prototype.stripPrefix = function(prefix) {
+  return prefix.uncons.matches({
+    empty: () => Optional.ofNonNull([...this]),
+    present: ([x, xs]) =>
+      this.uncons.matches({
+        empty: () => Optional.empty<any[]>(),
+        present: ([y, ys]) => (x === y ? ys.stripPrefix(xs) : Optional.empty())
+      })
+  });
+};
+
+Array.prototype.isPrefixOf = function(body) {
+  return this.uncons.matches({
+    empty: () => true,
+    present: ([x, xs]) =>
+      body.uncons.matches({
+        empty: () => false,
+        present: ([y, ys]) => (x === y ? xs.isPrefixOf(ys) : false)
+      })
+  });
+};
+
+Array.prototype.isSuffixOf = function(body) {
+  return this.unsnoc.matches({
+    empty: () => true,
+    present: ([xs, x]) =>
+      body.unsnoc.matches({
+        empty: () => false,
+        present: ([ys, y]) => (x === y ? xs.isSuffixOf(ys) : false)
+      })
+  });
+};
+
+Array.prototype.isInfixOf = function(body) {
+  return this.uncons.matches({
+    empty: () => true,
+    present: ([x, xs]) =>
+      body.uncons.matches({
+        empty: () => false,
+        present: ([y, ys]) => (x === y ? xs.isPrefixOf(ys) : this.isInfixOf(ys))
+      })
+  });
+};
+
+Array.prototype.isSubsequenceOf = function(body) {
+  return this.uncons.matches({
+    empty: () => true,
+    present: ([x, xs]) =>
+      body.uncons.matches({
+        empty: () => false,
+        present: ([y, ys]) =>
+          x === y ? xs.isSubsequenceOf(ys) : this.isSubsequenceOf(ys)
+      })
+  });
+};
+
+Array.prototype.partition = function(pred) {
+  return [this.filter(pred), this.filter(e => !pred(e))];
+};
+
+Array.prototype.includesIndex = function(target) {
+  const index = this.findIndex(e => e === target);
+  if (index === -1) {
+    return Optional.empty();
+  } else {
+    return Optional.ofNonNull(index);
+  }
+};
+
+Array.prototype.includesIndices = function(target) {
+  return this.findIndices(e => e === target);
+};
+
+Array.prototype.findLastIndex = function(pred) {
+  for (let i = this.length; 0 < i; --i) {
+    if (pred(this[i])) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+Array.prototype.findIndices = function(pred) {
+  const indices: number[] = [];
+  this.forEach((element, index) => {
+    if (pred(element)) {
+      indices.push(index);
+    }
+  });
+  return indices;
+};
+
+Array.prototype.groupBy = function(pred) {
+  return this.uncons.matches({
+    empty: () => [],
+    present: ([x, xs]) => {
+      const [ys, zs] = xs.span(e => pred(e, x));
+      return cons(cons(x, ys), zs.groupBy(pred));
+    }
+  });
 };
